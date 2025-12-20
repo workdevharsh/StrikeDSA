641bdbfd8d37cba4e89ff1247518a19176aa9755
workdevharsh <workharsh99@gmail.com>
2025-12-20 12:54:31 +0530

    Learning of basics of array

---
+++ L-07/index.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int mark[5] = {70,80,40,50,90};
+
+    for(int i=0; i<5; i++){
+        cout<< mark[i] <<endl;
+    }
+    return 0;
+}
