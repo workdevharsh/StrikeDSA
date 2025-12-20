dab98c12d3773cfb07f179cef03677c129735fb1
workdevharsh <workharsh99@gmail.com>
2025-12-20 13:34:51 +0530

    Learning of basics of array and linear algorithm

--- L-07/index.cpp
+++ L-07/index.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 int main() {
     int mark[5] = {70,80,40,50,90};
+    int sum = 0;
+    // for(int i=0; i<5; i++){
+    //     cout<< mark[i] <<endl;
+    // }
+    
+    // for(int i=0; i<5; i++){
+    //     sum+=mark[i];
+    // }
+    // cout<< sum/5 << endl;
 
+    // int heightest = mark[0];
+    // for(int i=0; i<5; i++){
+    //     if(mark[i]>heightest){
+    //         heightest = mark[i];
+    //     }
+    // }
+    // cout << heightest << endl;
+
+    int student_mark = 50;
     for(int i=0; i<5; i++){
-        cout<< mark[i] <<endl;
+        if(student_mark==mark[i]){
+            cout << "YES";
+            break;
+        }
     }
     return 0;
 }
